4bcd6b329aa3095fa89d84cdfd8ff279c906de3e
xunknown <xunknown@163.com>
2019-10-20 13:01:09 +0800

    Test
    Signed-off-by: xunknown <xunknown@163.com>

--- dp-coin/dp-coin-01.c
+++ dp-coin/dp-coin-01.c
@@ -1,3 +1,9 @@
+/*
+ * 题目描述：
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+ * 如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 假设每种硬币的数量是无限的。
+ */
 #include <stdio.h>
 
 #define M 100
@@ -7,14 +13,17 @@
 static int amount[M];
 static int coins[N];
 
-int main (void)
+int main(void)
 {
 	int m, n;
-	if (scanf("%d %d", &m, &n) != 2) return 0;
-	if (m < 1 || m > M || n < 1 || n > N) return 0;
+	if (scanf("%d %d", &m, &n) != 2)
+		return 0;
+	if (m < 1 || m > M || n < 1 || n > N)
+		return 0;
 	printf("%d %d\n", m, n);
 	for (int i = 0; i < n; i++) {
-		if (scanf("%d", &coins[i]) != 1) break;
+		if (scanf("%d", &coins[i]) != 1)
+			break;
 	}
 	for (int i = 0; i < n; i++) {
 		printf("%d\n", coins[i]);
@@ -22,6 +31,7 @@ int main (void)
 	amount[0] = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = coins[i]; j <= m; j++) {
+			// amount[j] = min(amount[j], amount[j - coins[i]] + 1);
 			amount[j] = min(amount[j], amount[j - coins[i]]) + 1;
 		}
 	}
--- dp-coin/dp-coin-02.c
+++ dp-coin/dp-coin-02.c
@@ -1,3 +1,9 @@
+/*
+ * 题目：
+ * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。
+ * 假设每一种面额的硬币只有1个。
+ */
+
 #include <stdio.h>
 
 #define M 100
@@ -9,11 +15,14 @@ static int coins[N];
 int main (void)
 {
 	int m, n;
-	if (scanf("%d %d", &m, &n) != 2) return 0;
-	if (m < 1 || m > M || n < 1 || n > N) return 0;
+	if (scanf("%d %d", &m, &n) != 2)
+		return 0;
+	if (m < 1 || m > M || n < 1 || n > N)
+		return 0;
 	printf("%d %d\n", m, n);
 	for (int i = 0; i < n; i++) {
-		if (scanf("%d", &coins[i]) != 1) break;
+		if (scanf("%d", &coins[i]) != 1)
+			break;
 	}
 	for (int i = 0; i < n; i++) {
 		printf("%d\n", coins[i]);
@@ -21,7 +30,7 @@ int main (void)
 	amount[0] = 1;
 	for (int i = 0; i < n; i++) {
 		for (int j = m; j >= coins[i]; j--) {
-			amount[j] = amount[j] + amount[j - coins[i]];
+			amount[j] = amount[j] + (j == coins[i] ? 1 : amount[j - coins[i]]);
 		}
 	}
 	printf("%d\n", amount[m]);
